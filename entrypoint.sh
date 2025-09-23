23538c688a749c58b75845823e2683d473cecf44
sta-crux <m.t.n@hotmail.it>
2025-09-23 19:04:05 +0200

    clean up files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Ensure /home/keyKeeper exists and is writable by appuser
-mkdir -p /home/keyKeeper
-chown -R appuser:appuser /home/keyKeeper
-
-# Run the bot as appuser
-exec su-exec appuser java -Duser.home=/home/ -jar /home/keykeeper.jar
